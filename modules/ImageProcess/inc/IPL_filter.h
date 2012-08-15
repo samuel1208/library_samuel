25f1471668b4f8c7044e70d2efb3ce5568c58f89
fshen <sf2252@arcsoft.com>
2012-08-15 16:14:42 +0800

    update

--- modules/ImageProcess/inc/IPL_filter.h
+++ modules/ImageProcess/inc/IPL_filter.h
@@ -5,20 +5,6 @@
 
 #include "define.h"
 
-enum COLOR_CVT_FLAG
-{
-	IPL_RGB2HSL  = 0,
-	IPL_RGB2HSV  = 1,
-	IPL_RGB2GRAY = 2,
-};
-
-enum RESIZE_FLAG
-{
-	IPL_RESIZE_INTERPOLATION_BILINEAR  = 0,
-};
-
-
-
 DLL_EXPORTS int  MedianFilter(
 						      const unsigned char *srcImg , int srcStep,
 				              unsigned char *dstImg,  int dstStep,
@@ -29,15 +15,7 @@ DLL_EXPORTS int  GaussianFilter(const unsigned char *srcImg , int srcStep,
 							    unsigned char *dstImg,  int dstStep,
 							    int width, int height,  int channel,
 							    float *gauss_kernel,    int kernelSize, float kernelSum);
-DLL_EXPORTS int  CVTColor(
-					      const unsigned char *srcImg , int srcStep,
-				          unsigned char *dstImg,  int dstStep,
-				          int width, int height,  int CVTFormat
-					 );
 
-DLL_EXPORTS int  Resize(const unsigned char *srcImg, int srcWidth, int srcHeight, int srcStep,
-					    unsigned char *dstImg, int dstWidth, int dstHeight, int dstStep,
-					    float xScale, float yScale, int channel, int InterpolationFlag );
 
 #endif  
 
